45acbaaccd54d14be78514dbb3a717111d3ea2ea
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-19 21:39:47 +0530

    Changed global text theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import './home.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -12,16 +11,29 @@ class MyApp extends StatelessWidget {
     var _secondaryLight = Color(0xffe1e1e1);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Palette Camera',
       theme: ThemeData(
         primaryColor: _primaryLight,
-        appBarTheme: AppBarTheme(color: _secondaryLight, iconTheme: IconThemeData(color: _primaryLight)),
-          buttonTheme: ButtonThemeData(buttonColor: _primaryLight, textTheme: ButtonTextTheme.primary,),
-          fontFamily: 'Fira Sans',
-          textTheme: TextTheme(
-              title: TextStyle(fontSize: 36.0, fontFamily: 'Sacramento', color: _primaryLight),
-              headline: TextStyle(fontSize: 50),
-              body1: TextStyle(fontSize: 18))),
+        appBarTheme: AppBarTheme(
+            color: _secondaryLight,
+            iconTheme: IconThemeData(color: _primaryLight)),
+        buttonTheme: ButtonThemeData(
+          buttonColor: _primaryLight,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        fontFamily: 'Fira Sans',
+        textTheme: TextTheme(
+          title: TextStyle(
+              fontSize: 36.0, fontFamily: 'Sacramento', color: _primaryLight),
+          headline: TextStyle(
+              fontSize: 24, color: _primaryLight, fontWeight: FontWeight.bold),
+          subhead: TextStyle(
+              fontSize: 18, color: _primaryLight, fontWeight: FontWeight.bold),
+          caption: TextStyle(fontSize: 14),
+          body1: TextStyle(fontSize: 18),
+        ),
+      ),
       home: Home(),
     );
   }

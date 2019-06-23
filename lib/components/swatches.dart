94fe3f99b9e3c588e5f354a40124de8a27ef8c43
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-23 20:05:45 +0530

    HSL and RGB block in ColorBlock

--- lib/components/swatches.dart
+++ lib/components/swatches.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:tinycolor/tinycolor.dart';
 
@@ -48,15 +49,29 @@ class Swatches extends StatelessWidget {
   }
 }
 
-class ColorBlock extends StatelessWidget {
+class ColorBlock extends StatefulWidget {
   const ColorBlock({this.color, this.label});
 
   final Color color;
   final String label;
 
   @override
+  _ColorBlockState createState() => _ColorBlockState();
+}
+
+class _ColorBlockState extends State<ColorBlock> {
+  bool _hslValue = false;
+  bool _rgbValue = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getColorFormatPreferences();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (color == null) {
+    if (widget.color == null) {
       return Container();
     }
 
@@ -64,34 +79,93 @@ class ColorBlock extends StatelessWidget {
       direction: Axis.horizontal,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Center(
-              child: Text(
-                getHexFromColor(color),
-                style: TextStyle(
-                    color: getOppositeColor(color),
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5),
-              ),
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[renderHEX(), renderRGB(), renderHSL()],
             ),
-            color: color,
-            height: 70.0,
-            width: MediaQuery.of(context).size.width - 20,
           ),
+          color: widget.color,
+          width: MediaQuery.of(context).size.width,
         ),
       ],
     );
+  }
+
+  Widget renderHEX() {
+    return Text(
+      getHexFromColor(widget.color),
+      style: TextStyle(
+        color: getOppositeColor(widget.color),
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 1.5,
+      ),
+    );
+  }
+
+  Widget renderRGB() {
+    if (_rgbValue == true) {
+      return Text(
+        getRgbFromColor(widget.color),
+        style: TextStyle(
+          color: getOppositeColor(widget.color),
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+        ),
+      );
+    }
+    return Container();
+  }
+
+  Widget renderHSL() {
+    if (_hslValue == true) {
+      return Text(
+        getHSLFromColor(widget.color),
+        style: TextStyle(
+          color: getOppositeColor(widget.color),
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+        ),
+      );
+    }
+    return Container();
   }
 
   String getHexFromColor(Color color) {
     return "#" + color.toString().substring(10, 16).toUpperCase();
   }
 
+  String getRgbFromColor(Color color) {
+    int red = color.red;
+    int green = color.green;
+    int blue = color.blue;
+
+    return 'RGB($red,$green,$blue)';
+  }
+
+  String getHSLFromColor(Color color) {
+    String hue = HSLColor.fromColor(color).hue.toStringAsFixed(1);
+    String saturation = HSLColor.fromColor(color).saturation.toStringAsFixed(1);
+    String lightness = HSLColor.fromColor(color).lightness.toStringAsFixed(1);
+
+    return 'HSL($hue, $saturation, $lightness)';
+  }
+
   Color getOppositeColor(Color color) {
     if (TinyColor(color).isDark()) return Colors.white;
     return Colors.black;
+  }
+
+  _getColorFormatPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _rgbValue = (prefs.getBool('rgb')) ?? false;
+      _hslValue = (prefs.getBool('hsl')) ?? false;
+    });
   }
 }
